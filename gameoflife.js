b9944314784859b71212393bd7580d508233ded4
192Iridium77 <matt.martin0108@live.com>
2017-10-07 13:00:21 +1030

    functionalization of main processing loop

--- gameoflife.js
+++ gameoflife.js
@@ -1,4 +1,4 @@
-/** RULES:
+/* RULES:
  *  1. Any live cell with fewer than two live neighbours dies,
  *    as if caused by underpopulation.
  *  2. Any live cell with two or three live neighbours lives
@@ -7,7 +7,7 @@
  *    as if by overpopulation.
  *  4. Any dead cell with exactly three live neighbours becomes a live cell,
  *    as if by reproduction.
- **/
+ */
 
 
 // set up matrix method so an m by n board can be initialized with constant a
@@ -26,39 +26,31 @@ Array.matrix = function(m,n,constant) {
 
 // create game of life board
 var dim = 5;
-// TODO make sure this works equivalently
-// var board = Array.matrix(dim, dim, 0);
+var board = Array.matrix(dim, dim, 0);
 
-var board = [[0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0],
-            [0,0,0,0,0]];
-
-board[2][3] = 1;
-board[2][2] = 1;
+board[2][3] = 2;
+board[2][2] = 4;
 board[1][2] = 1;
-console.table(board); // TODO problem: array is being printed as all zeros!?!?!
+console.table(board); // TODO problem: every iteration of the array is being printed as all zeros!?!?!
 
-// begin timestep loop
-// TODO check what happens when you don't run this
-// TODO debug this loop using console.log
-for (var t = 0; t < 4; t++) {
-  // iterate board
-  var board_copy = board;
-  for (i = 0; i < board.length; i++) {
-    for (j = 0; j < board[i].length; j++) {
-      var n = count_neighbors(board_copy, i, j);
-      console.log(n);
-      if (board_copy[i][j] == 1 && check_condition1(n)) {
-        board[i][j] = 0;
-      }
-      if (board_copy[i][j] == 0 && check_condition2(n)) {
-        board[i][j] = 1;
+function process_board (board) {
+  for (var t = 0; t < 4; t++) {
+    // iterate board
+    var board_copy = board;
+    for (i = 0; i < board.length; i++) {
+      for (j = 0; j < board[i].length; j++) {
+        var n = count_neighbors(board_copy, i, j);
+        //console.log(n);
+        if (board_copy[i][j] == 1 && check_condition1(n)) {
+          board[i][j] = 0;
+        }
+        if (board_copy[i][j] == 0 && check_condition2(n)) {
+          board[i][j] = 1;
+        }
       }
     }
+    //console.table(board);
   }
-  console.table(board);
 }
 
 // implement rule1, check given position against a copy of the board
@@ -134,3 +126,6 @@ function display_board(board, dim) {
   }
   document.getElementById("array4").innerHTML = "-".repeat(2*dim + 1) + "<br>";
 };
+
+process_board(board);
+
